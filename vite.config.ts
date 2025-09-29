a7e4e797eaf7c0d61cf7402abe086f9704c79cc0
Stéphane Aravit <stephane.aravit@example.com>
2025-09-29 18:59:26 +0200

    Désactivation wayfinder en production

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,11 @@ export default defineConfig({
             refresh: true,
         }),
         tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
+        process.env.NODE_ENV !== 'production' // désactivation de wayfinder en prod car problème sur railway
+            ? wayfinder({
+                  formVariants: true,
+              })
+            : null,
         vue({
             template: {
                 transformAssetUrls: {
@@ -23,5 +25,5 @@ export default defineConfig({
                 },
             },
         }),
-    ],
+    ].filter(Boolean), // supprime les nulls pour prod,
 });
